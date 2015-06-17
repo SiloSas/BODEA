d6dde0df2e130dcda67cd89fecd465f525f746cd
Loann Delgado <loann.delgado@gmail.com>
2015-06-17 15:28:59 +0200

    started commandes

--- app/scripts/controllers/commandesCtrl.js
+++ app/scripts/controllers/commandesCtrl.js
@@ -1,4 +1,31 @@
 angular.module('bodeaApp')
     .controller('CommandesCtrl', function ($scope) {
+    $scope.commandes = [
+        {
+            id: 1,
+            store: 'magasin1',
+            date: '10/11/11',
+            state: 1
+        },
+        {
+            id: 3,
+            store: 'magasin2',
+            date: '10/11/11',
+            state: 2
+        },
+        {
+            id: 2,
+            store: 'magasin2',
+            date: '10/11/11',
+            state: 2
+        }
+    ];
+    $scope.predicate = 'date';
+    $scope.reverse = false;
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    };
 
-    })
+
+    });
